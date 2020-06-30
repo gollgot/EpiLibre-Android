e44abadb896a86669c7f0c4a31838fae16650397
Loïc Dessaules <loic.dessaules@gmail.com>
2020-06-30 23:33:07 +0200

    Add guest menu with signin activity

--- app/src/main/java/ch/epilibre/epilibre/MainActivity.java
+++ app/src/main/java/ch/epilibre/epilibre/MainActivity.java
@@ -2,7 +2,11 @@ package ch.epilibre.epilibre;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.guest_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Handle item selection
+        switch (item.getItemId()) {
+            case R.id.itemGuestMenuSignIn:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
