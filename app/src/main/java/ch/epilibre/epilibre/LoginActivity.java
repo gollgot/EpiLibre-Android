bb226043e02818f2528fdafb62c4a925bdb625f1
Loïc Dessaules <loic.dessaules@gmail.com>
2020-07-01 10:32:04 +0200

    Add material design and update login activity design

--- app/src/main/java/ch/epilibre/epilibre/LoginActivity.java
+++ app/src/main/java/ch/epilibre/epilibre/LoginActivity.java
@@ -3,6 +3,10 @@ package ch.epilibre.epilibre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button) findViewById(R.id.loginBtnConnection);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(findViewById(android.R.id.content), "Login", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
