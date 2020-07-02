95fdabc6c1b5d890da8e693b5b466f9f944f757f
Loïc Dessaules <loic.dessaules@gmail.com>
2020-07-02 13:51:17 +0200

    Add dialog box if shopping list not empty when back button pressed

--- app/src/main/java/ch/epilibre/epilibre/MainActivity.java
+++ app/src/main/java/ch/epilibre/epilibre/MainActivity.java
@@ -1,11 +1,13 @@
 package ch.epilibre.epilibre;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.DragEvent;
@@ -17,6 +19,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -123,6 +126,38 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    @Override
+    public void onBackPressed()
+    {
+        // Shopping list not empty -> display dialog box to be sure to quit the app
+        // Because back button will stop the app and call again OnCreate() at comeback,
+        // so we'll lost the shopping list
+        if(!shoppingList.isEmpty()){
+            new MaterialAlertDialogBuilder(MainActivity.this)
+                .setTitle("Liste d'achats en cours")
+                .setMessage("Voulez-vous vraiment quitter l'application ?")
+                .setPositiveButton("Quitter", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        MainActivity.super.onBackPressed();
+                    }
+                })
+                .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) { }
+                })
+                .show();
+        }
+        // Shopping list empty, no problem
+        else{
+            super.onBackPressed();
+        }
+    }
+
+
+
+    /********* MENU MANAGEMENT *********/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
